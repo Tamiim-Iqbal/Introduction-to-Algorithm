1b12297a4b6329f04bfc9b8681f1fcae5234c751
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-01-26 16:34:53 +0600

    Module 3

---
+++ 3-Depth First Search (DFS) & 2D Grid /component.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5;
+vector<int> v[N];
+bool vis[N];
+void dfs(int src)
+{
+    cout<<src<<endl;
+    vis[src] = true;
+    for (int child: v[src])
+    {
+        if(vis[child] == false)
+        {
+            //vis[child] = true;
+            dfs(child);
+        }
+    }
+
+}
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    while(e--)
+    {
+        int a,b;
+        cin>>a>>b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    memset(vis,false,sizeof(vis));
+    int count = 0;
+    for(int i=0; i<n; i++)
+    {
+        if(vis[i]==false)
+        {
+            dfs(i);
+            count++;
+        }
+    }
+    cout<<"Compopent : "<<count<<endl;
+    return 0;
+}
