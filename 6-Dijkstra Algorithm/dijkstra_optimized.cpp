ed43000a0aa1e433af98240ea448bac754ec2c0d
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-02-11 02:46:36 +0600

    Module 6

---
+++ 6-Dijkstra Algorithm/dijkstra_optimized.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 100;
+int dis[N];
+vector<pair<int,int>>v[N];
+class cmp
+{
+    public:
+        bool operator()(pair<int,int> a, pair<int,int> b)
+        {
+            return a.second > b.second;
+        }
+
+};
+void dijkstra(int src)
+{
+    priority_queue<pair<int,int>,vector<pair<int,int>>,cmp> pq;
+    pq.push({src,0});
+    dis[src]=0;
+    while(!pq.empty())
+    {
+        pair<int,int> par = pq.top();
+        pq.pop();
+        int node = par.first;
+        int cost = par.second;
+        for(pair<int,int> c: v[node])
+        {
+            int childNode = c.first;
+            int childCost = c.second;
+            if(cost+childCost < dis[childNode])
+            {
+                //path relax
+                dis[childNode] = cost+childCost;
+                pq.push({childNode,dis[childNode]});
+            }
+        }
+    }
+}
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    while (e--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        v[a].push_back({b,c});
+        v[b].push_back({a,c});
+    }
+    for(int i=0; i<n; i++)
+    {
+        dis[i]=INT_MAX;
+    }
+    dijkstra(0);
+    for(int i=0; i<n; i++)
+    {
+        cout<<i<<"->"<<dis[i]<<endl;
+    }
+    return 0;
+}
